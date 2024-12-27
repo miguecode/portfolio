b23c563f41b78201d8ff1567bcf8e253d452cbb3
Leumig <junmigue7@gmail.com>
2024-12-27 03:41:29 -0300

    Estructura de proyectos y cambio de azul por violeta

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,27 +1,31 @@
+import { technologysDataOne } from "../data/technologysData";
+import { technologysDataTwo } from "../data/technologysData";
+import { technologysDataThree } from "../data/technologysData";
+
 // Projects data
 export const projects = [
   {
     title: "Proyecto 1",
     description: "Esta es la descripción del proyecto 1",
+    image: "#",
     link: "#",
     github: "#",
-    image: "#",
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [technologysDataOne[4], technologysDataTwo[1]]
   },
   {
     title: "Proyecto 2",
     description: "Esta es la descripción del proyecto 2",
+    image: "#",
     link: "#",
     github: "#",
-    image: "#",
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [technologysDataOne[4], technologysDataTwo[1]]
   },
   {
     title: "Proyecto 3",
     description: "Esta es la descripción del proyecto 3",
+    image: "#",
     link: "#",
     github: "#",
-    image: "#",
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [technologysDataOne[4], technologysDataTwo[1]]
   },
 ];
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,32 +5,37 @@ export default {
   theme: {
     extend: {
       colors: {
-        "navbar-primary": "#2557fc",
-        "navbar-secondary": "#67a2ea",
+        // Blue Theme
+        // "navbar-primary": "#2557fc",
+        // "navbar-secondary": "#67a2ea",
+
+        // Purple Theme
+        "navbar-primary": "#8927f9",
+        "navbar-secondary": "#b968e8",
         "navbar-dark-bg": "#212031",
-        // Technology's Primary colors
+
+        // Technology's colors
         "html-primary": "#f06b51",
+        "html-secondary": "#a04231",
         "css-primary": "#0396DE",
+        "css-secondary": "#c9d1e2",
         "javascript-primary": "#f0db4f",
+        "javascript-secondary": "#f0db4f",
         "typescript-primary": "#5b92d1",
+        "typescript-secondary": "#c9d8e0",
         "angular-primary": "#ec3c88",
+        "angular-secondary": "#42001c",
         "tailwind-primary": "#38BDF8",
+        "tailwind-secondary": "#85d5f7",
         "bootstrap-primary": "#8b6bba",
+        "bootstrap-secondary": "#c9bae0",
         "firebase-primary": "#FFCD32",
+        "firebase-secondary": "#915900",
         "astro-primary": "#ccd9ff",
         "php-primary": "#a6a9c4",
+        "php-secondary": "#ccd3e8",
         "nodejs-primary": "#6da565",
         "ionic-primary": "#3880FF",
-        // Technology's Secondary colors
-        "html-secondary": "#a04231",
-        "css-secondary": "#c9d1e2",
-        "javascript-secondary": "#f0db4f",
-        "typescript-secondary": "#c9d8e0",
-        "angular-secondary": "#42001c",
-        "tailwind-secondary": "#85d5f7",
-        "bootstrap-secondary": "#c9bae0",
-        "firebase-secondary": "#915900",
-        "php-secondary": "#ccd3e8",
       },
     },
   },
