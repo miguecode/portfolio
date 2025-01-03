5e853cee2803428977dcd4b1c76a6b94a4137af3
Leumig <junmigue7@gmail.com>
2025-01-03 03:48:59 -0300

    Textos destacados mejorados y ordenamiento de código

--- src/data/personalData.ts
+++ src/data/personalData.ts
@@ -10,7 +10,6 @@ export const data = {
   title: "Hola, soy Miguel",
   subTitle:
      "<span class='highlight-1'>Desarrollador Frontend</span> con formación Universitaria en Programación y conocimientos en tecnologías web modernas.",
-    // "<code><pre>Desarrollador Frontend</pre></code> con formación Universitaria en Programación y conocimientos en tecnologías web modernas.",
   perfilImage: "../../src/assets/profile-pic.webp",
   badge: "¡Disponible para trabajar!",
   email: "junmigue7@gmail.com",
@@ -22,10 +21,10 @@ export const data = {
 // About me data
 export const aboutMeData = {
   firstParagraph:
-    "Mi nombre es Miguel Ángel Gil, tengo 23 años y vivo en Buenos Aires, Argentina. Empecé en la programación en 2021, cuando me inscribí en la Tecnicatura Universitaria en Programación en la UTN. Con el tiempo, decidí enfocarme al desarrollo Frontend y es en lo que me especializo actualmente.",
+    "Mi nombre es Miguel Ángel Gil, tengo 23 años y vivo en Buenos Aires, Argentina. <span class='highlight-2'>Empecé en la programación en 2021</span>, cuando me inscribí en la Tecnicatura Universitaria en Programación en la UTN. Con el tiempo, decidí enfocarme al <span class='highlight-2'>desarrollo Frontend</span> y es en lo que me especializo actualmente.",
   secondParagraph:
-    "Durante mi formación y práctica, he trabajado en el desarrollo web, tanto del lado del cliente como del servidor. También experimenté en aplicaciones móviles usando Ionic, trabajando tanto de manera individual como en equipo, lo que me hizo mejorar en habilidades de colaboración y comunicación.",
+    "Durante mi formación y práctica, he trabajado en el desarrollo web, <span class='highlight-2'>tanto del lado del cliente como del servidor</span>. También experimenté en aplicaciones móviles usando Ionic, <span class='highlight-2'>trabajando de manera individual y en equipo</span>, lo que me hizo mejorar en habilidades de colaboración y comunicación.",
   thirdParagraph:
-    "En 2024 obtuve mi título como Técnico Universitario en Programación. Actualmente, estoy trabajando en nuevos proyectos los cuales son sitios web relacionados a una de mis pasiones, el fútbol. Mi objetivo es mejorar en el desarrollo Frontend mientras me preparo para contribuir de forma profesional.",
+    "<span class='highlight-2'>En 2024 obtuve mi título como Técnico Universitario en Programación</span>. Actualmente, estoy trabajando en nuevos proyectos los cuales son sitios web relacionados a una de mis pasiones, el fútbol. Mi objetivo es <span class='highlight-2'>seguir mejorando y aprendiendo</span> mientras me preparo para contribuir de forma profesional.",
   image: "../../src/assets/profile-pic.webp",
 };
