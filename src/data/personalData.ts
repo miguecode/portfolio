7d3b03cafaf16e0a6a941b135fab46048a027efb
Leumig <junmigue7@gmail.com>
2025-01-10 18:48:26 -0300

    Perfil parcialmente responsive

--- src/data/personalData.ts
+++ src/data/personalData.ts
@@ -21,10 +21,10 @@ export const data = {
 // About me data
 export const aboutMeData = {
   firstParagraph:
-    "Mi nombre es Miguel Ángel Gil, tengo 23 años y vivo en Buenos Aires, Argentina. <span class='highlight-2'>Empecé en la programación en 2021</span>, cuando me inscribí en la Tecnicatura Universitaria en Programación en la UTN. Con el tiempo, decidí enfocarme al <span class='highlight-2'>desarrollo Frontend</span> y es en lo que me especializo actualmente.",
+    "Me llamo Miguel Ángel Gil, tengo 23 años y vivo en Buenos Aires, Argentina. <span class='highlight-2'>Empecé en la programación en 2021</span>, cuando me inscribí en la Tecnicatura Universitaria en Programación en la UTN. Con el tiempo, decidí enfocarme al <span class='highlight-2'>desarrollo Frontend</span> y es en lo que me especializo actualmente.",
   secondParagraph:
-    "Durante mi formación y práctica, he trabajado en el desarrollo web, <span class='highlight-2'>tanto del lado del cliente como del servidor</span>. También experimenté en aplicaciones móviles usando Ionic, <span class='highlight-2'>trabajando de manera individual y en equipo</span>, lo que me hizo mejorar en habilidades de colaboración y comunicación.",
+    "Durante mi formación y práctica, he trabajado en el desarrollo web, <span class='highlight-2'>tanto del lado del cliente como del servidor</span>. También experimenté en aplicaciones móviles, <span class='highlight-2'>trabajando de manera individual y en equipo</span>, lo que me hizo mejorar en habilidades de colaboración y comunicación.",
   thirdParagraph:
-    "<span class='highlight-2'>En 2024 obtuve mi título como Técnico Universitario en Programación</span>. Actualmente, estoy trabajando en nuevos proyectos los cuales son sitios web relacionados a una de mis pasiones, el fútbol. Mi objetivo es <span class='highlight-2'>seguir mejorando y aprendiendo</span> mientras me preparo para contribuir de forma profesional.",
+    "<span class='highlight-2'>En 2024 obtuve mi título como Técnico Universitario en Programación</span>. Actualmente, estoy trabajando en nuevos proyectos relacionados a una de mis pasiones, el fútbol. Mi objetivo es <span class='highlight-2'>contribuir de forma profesional</span> para brindar soluciones y encarar nuevos desafíos.",
   image: "/images/profile-pic.webp",
 };
